8f2cecb15bf577aaddef01a11c7371fbdd26c82b
yamasaki <yamasakipann0218@gmail.com>
2022-12-04 03:40:26 +0900

    insert sql

---
+++ server/database/mysql/sql/2_insert_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO rooms (`id`, `name`,`max_member`, `member_count`) VALUES ("c1", "n1", 1,1);
+INSERT INTO rooms (`id`, `name`,`max_member`, `member_count`) VALUES ("c2", "n2", 2,2);
+INSERT INTO rooms (`id`, `name`,`max_member`, `member_count`) VALUES ("c3", "n3", 3,3);
+
