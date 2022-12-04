f53960bfb7e739fb2adb35ce60fcec5b84dbe8f6
yamasaki <yamasakipann0218@gmail.com>
2022-12-04 21:21:33 +0900

    member sql

---
+++ server/database/mysql/sql/4_insert_member_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO members (`name`,`room_id`) VALUES ("testname1", "c1");
+INSERT INTO members (`name`,`room_id`) VALUES ("testname2", "c2");
+INSERT INTO members (`name`,`room_id`) VALUES ("testname3", "c3");
