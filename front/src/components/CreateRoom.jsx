348af900a94c19da011b03850b40018777049389
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2022-12-07 22:02:46 +0900

    fix styles

--- front/src/components/CreateRoom.jsx
+++ front/src/components/CreateRoom.jsx
@@ -5,8 +5,8 @@ const CreateRoom = () => {
   return (
     <div class="w-screen h-screen bg-slate-300 justify-center flex text-center">
       <div class="w-1/2 h-3/5 bg-white my-auto rounded-2xl">
-        <p class="text-6xl pt-16">Create Room</p>
-        <div class="py-28">
+        <p class="text-6xl py-24">Create Room</p>
+        <div class="py-20">
           <button 
           class="text-4xl text-black 
            w-52 h-24 my-auto
