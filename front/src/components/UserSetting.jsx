b93cfe6de42f4a7b411f2b0bd9e7405c33448106
Yuri-Moriyama <m_keanu4@docomo.ne.jp>
2022-12-08 00:46:36 +0900

    front-create-usersetting

--- front/src/components/UserSetting.jsx
+++ front/src/components/UserSetting.jsx
@@ -1,7 +1,20 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const UserSetting = () => {
     return(
-        <div>
-            hello, world
+        <div class="w-screen h-screen bg-slate-300 justify-center flex text-center">
+            <div class="w-1/2 h-128 bg-white my-auto rounded-2xl">
+                <p class="text-6xl py-24 font-bold">User</p>
+                <div class="py-20">
+                    <button 
+                    class="text-4xl text-black 
+                    w-52 h-24 my-auto
+                    bg-cyan-500 rounded-2xl hover:bg-cyan-300">
+                        Chat
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
