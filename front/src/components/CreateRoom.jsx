15152a1939f22566cecc855688c1fcf6c95a30c0
Yuri-Moriyama <m_keanu4@docomo.ne.jp>
2022-12-08 00:00:27 +0900

    fix-font-bold

--- front/src/components/CreateRoom.jsx
+++ front/src/components/CreateRoom.jsx
@@ -5,7 +5,7 @@ const CreateRoom = () => {
   return (
     <div class="w-screen h-screen bg-slate-300 justify-center flex text-center">
       <div class="w-1/2 h-128 bg-white my-auto rounded-2xl">
-        <p class="text-6xl py-24 fontbold-">Create Room!</p>
+        <p class="text-6xl py-24 font-bold">Create Room!</p>
         <div class="py-20">
           <button 
           class="text-4xl text-black 
