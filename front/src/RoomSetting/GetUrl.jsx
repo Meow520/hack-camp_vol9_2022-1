9bcec5074583f40f61d33f32249eae48ba28b50d
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2022-12-08 22:52:08 +0900

    create copy button

--- front/src/RoomSetting/GetUrl.jsx
+++ front/src/RoomSetting/GetUrl.jsx
@@ -2,14 +2,35 @@ import React, { useState } from "react";
 import "../styles/App.css";
 
 const GetURL = () => {
-  
+  const id = 1;
+  const url = `https://hogehoge.vercel.app/chat/${id}`;
+  const [visible, setVisible] = useState(false);
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(url);
+    setVisible(true);
+  };
+
   return (
     <div className="w-screen h-screen bg-slate-300 justify-center flex text-center">
       <div className="w-1/2 h-128 bg-white my-auto rounded-2xl">
-        <p className="text-6xl py-16 font-bold">Get URL</p>
-        <p className="text-xl py-16">Share the URL with your friends!</p>
+        <p className="text-6xl pt-24 pb-12 font-bold">Get URL</p>
+        <p className="text-xl">Share the URL with your friends!</p>
         <div className="py-10">
-        <p className="text-3xl py-16">URL</p>
+          <div className="py-6">
+            <a href={url} className="text-2xl p-3 h-18">
+              {url}
+            </a>
+          </div>
+          <div>
+            <button
+              className="text-xl text-slate-500 bg-slate-50 hover:bg-slate-200 w-32 h-18 border rounded-sm"
+              onClick={copy}
+            >
+              Copy
+            </button>
+          </div>
+          {visible && <p className="text-xl">Copied</p>}
         </div>
       </div>
     </div>
