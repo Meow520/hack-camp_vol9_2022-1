709ee086927897c54a06d96f53fdecb8dd2ba0fd
39TO <ohisama.0610@icloud.com>
2022-12-08 23:17:22 +0900

    button component 作成

---
+++ front/src/components/atoms/LinkButton.jsx
@@ -0,0 +1,11 @@
+export const LinkButton = ({ label, color, path, disabled }) => {
+  const className = `${color} ${"items-center rounded-md px-4 py-2 w-10"}`;
+
+  return (
+    <a href={path} className="w-10">
+      <span className={className} tabIndex={-1} aria-disabled={disabled}>
+        {label}
+      </span>
+    </a>
+  );
+};
