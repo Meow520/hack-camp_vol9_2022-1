c8c923e34ed14e101b94b016b7811d334e23d4ec
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2022-12-10 02:00:33 +0900

    delete ws-related files

--- front/src/components/chat/Chatting.jsx
+++ front/src/components/chat/Chatting.jsx
@@ -1,12 +1,7 @@
-import { MessageInput } from "./MessageInput";
-import { MessageList } from "./MessageList";
-
 const Chatting = () => {
   return (
     <div>
       <h1>Simple Chat</h1>
-      <MessageInput />
-      <MessageList />
     </div>
   );
 };
