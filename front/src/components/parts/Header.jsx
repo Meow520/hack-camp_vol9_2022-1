d6772ebedbd4cb103fa976cd87e02f15d3fa6981
Yuri-Moriyama <m_keanu4@docomo.ne.jp>
2022-12-11 12:44:46 +0900

    fix-header

--- front/src/components/parts/Header.jsx
+++ front/src/components/parts/Header.jsx
@@ -1,5 +1,11 @@
+import "../../styles/App.css"
+
+
 export const Header = ({name}) => {
   return (
-    <div>Header</div>
+      <div 
+        className = "text-xl text-white text-center font-bold bg-blue-700 opacity-80 py-3 ">
+        消えちゃっと-{name} room
+      </div>      
   )
 }
